792b5ab8f33146c837cfc29cb941a02776c45170
vphong-bos <phong.vu@bos-semi.com>
2026-04-06 14:08:36 +0000

    x

--- evaluation/eval_dataset.py
+++ evaluation/eval_dataset.py
@@ -12,53 +12,19 @@ def build_dataset(cfg, default_args=None):
     return build_from_cfg(cfg, DATASETS, default_args)
 
 
-# def extract_data(data):
-#     """Extract tensor payloads from MMCV DataContainer fields."""
-#     data["img_metas"] = data["img_metas"][0].data
-#     data["gt_bboxes_3d"] = data["gt_bboxes_3d"][0].data
-#     data["gt_labels_3d"] = data["gt_labels_3d"][0].data
-#     data["img"] = data["img"][0].data
-#     data["ego_his_trajs"] = data["ego_his_trajs"][0].data
-#     data["ego_fut_trajs"] = data["ego_fut_trajs"][0].data
-#     data["ego_fut_cmd"] = data["ego_fut_cmd"][0].data
-#     data["ego_lcf_feat"] = data["ego_lcf_feat"][0].data
-#     data["gt_attr_labels"] = data["gt_attr_labels"][0].data
-#     data["map_gt_labels_3d"] = data["map_gt_labels_3d"].data[0]
-#     data["map_gt_bboxes_3d"] = data["map_gt_bboxes_3d"].data[0]
-#     return data
-
-def unwrap_datacontainer(x):
-    while hasattr(x, "data"):
-        x = x.data
-    return x
-
-def unwrap_singleton_list(x):
-    while isinstance(x, list) and len(x) == 1:
-        x = x[0]
-    return x
-
 def extract_data(data):
-    data = dict(data)
-
-    keys = [
-        "img_metas",
-        "gt_bboxes_3d",
-        "gt_labels_3d",
-        "img",
-        "ego_his_trajs",
-        "ego_fut_trajs",
-        "ego_fut_cmd",
-        "ego_lcf_feat",
-        "gt_attr_labels",
-        "map_gt_labels_3d",
-        "map_gt_bboxes_3d",
-    ]
-
-    for k in keys:
-        if k in data:
-            data[k] = unwrap_datacontainer(data[k])
-            data[k] = unwrap_singleton_list(data[k])
-
+    """Extract tensor payloads from MMCV DataContainer fields."""
+    data["img_metas"] = data["img_metas"][0].data
+    data["gt_bboxes_3d"] = data["gt_bboxes_3d"][0].data
+    data["gt_labels_3d"] = data["gt_labels_3d"][0].data
+    data["img"] = data["img"][0].data
+    data["ego_his_trajs"] = data["ego_his_trajs"][0].data
+    data["ego_fut_trajs"] = data["ego_fut_trajs"][0].data
+    data["ego_fut_cmd"] = data["ego_fut_cmd"][0].data
+    data["ego_lcf_feat"] = data["ego_lcf_feat"][0].data
+    data["gt_attr_labels"] = data["gt_attr_labels"][0].data
+    data["map_gt_labels_3d"] = data["map_gt_labels_3d"].data[0]
+    data["map_gt_bboxes_3d"] = data["map_gt_bboxes_3d"].data[0]
     return data
 
 def build_eval_loader(
