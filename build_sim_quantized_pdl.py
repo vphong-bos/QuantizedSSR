28995a83d635562331340d8f3b4478c8962c8853
vphong-bos <phong.vu@bos-semi.com>
2026-04-07 04:29:28 +0000

    x

--- build_sim_quantized_pdl.py
+++ build_sim_quantized_pdl.py
@@ -693,7 +693,7 @@ def main(args):
             onnx_path,
             input_names=["input"],
             output_names=["output"],
-            opset_version=17,
+            opset_version=20,
             export_int32_bias=False,
             prequantize_constants=True
         )
